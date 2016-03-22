f2e71d70a5df51a6a8a976b378eaa50916d4a819
Viredery <dyrviredery@hotmail.com>
2016-03-23 01:09:12 +0800

    LL,updating

--- RE_tree.cpp
+++ RE_tree.cpp
@@ -1,19 +1,99 @@
 #include "RE_tree.h"
 
 
-
 void insert_production_one(int type, RE_tree_node *p, char num)
 {
+	if(type == 1)
+	{
+		RE_tree_node *p_left = new RE_tree_node('|', p);
+		RE_tree_node *p_middle = new RE_tree_node(A_start, p);
+		RE_tree_node *p_right = new RE_tree_node(Regex_type, p);
+		p->lch_node = p_left;
+		p_left->bro_node = p_middle;
+		p_middle->bro_node = p_right;
+	}
+	else if(type == 2)
+		;
+	else
+		throw -1;
 }
 void insert_production_two(int type, RE_tree_node *p, char num)
 {
+	if(type == 1)
+	{
+		RE_tree_node *p_left = new RE_tree_node(B_type, p);
+		RE_tree_node *p_right = new RE_tree_node(A_type, p);
+		p->lch_node = p_left;
+		p_left->bro_node = p_right;
+	}
+	else if(type == 2)
+		;
+	else
+		throw -1;
 }
 void insert_production_three(int type, RE_tree_node *p, char num)
 {
+	if(type == 1)
+	{
+		RE_tree_node *p_left = new RE_tree_node(C_type, p);
+		RE_tree_node *p_right = new RE_tree_node('*', p);
+		p->lch_node = p_left;
+		p_left->bro_node = p_right;		
+	}
+	else if(type == 2)
+	{
+		RE_tree_node *p_new = new RE_tree_node(C_type, p);
+		p->lch_node = p_new;		
+	}
+	else
+		throw -1;
 }
 void insert_production_four(int type, RE_tree_node *p, char num)
 {
+	if(type == 1)
+	{
+		RE_tree_node *p_left = new RE_tree_node('(', p);
+		RE_tree_node *p_middle = new RE_tree_node(Regex_start, p);
+		RE_tree_node *p_right = new RE_tree_node(')', p);
+		p->lch_node = p_left;
+		p_left->bro_node = p_middle;
+		p_middle->bro_node = p_right;
+	}
+	else if(type == 2)
+	{
+		RE_tree_node *p_new = new RE_tree_node(e_type, p);
+		p_new->value = num;
+		p->lch_node = p_new;
+	}
+	else
+		throw -1;
 }
+void insert_production_five(int type, RE_tree_node *p, char num)
+{
+	RE_tree_node *p_left = new RE_tree_node(A_start, p);
+	RE_tree_node *p_right = new RE_tree_node(Regex_type, p);
+	p->lch_node = p_left;
+	p_left->bro_node = p_right;
+}
+void insert_production_six(int type, RE_tree_node *p, char num)
+{
+	RE_tree_node *p_left = new RE_tree_node(B_type, p);
+	RE_tree_node *p_right = new RE_tree_node(A_type, p);
+	p->lch_node = p_left;
+	p_left->bro_node = p_right;	
+}
+
+RE_tree_node::RE_tree_node(int t, RE_tree_node *p, char v, RE_tree_node *l, RE_tree_node *b)
+{
+	type = t;
+	prt_node = p;
+	lch_node = l;
+	bro_node = b;
+	value = v;
+}
+
+
+
 RE_tree::RE_tree()
 {
 	terminator.insert('*');
@@ -27,10 +107,34 @@ RE_tree::RE_tree()
 	insert.push_back(insert_production_three);
 	insert.push_back(insert_production_four);
 
-	root = new RE_tree_node;
-	root->type = Regex_type;
-	root->prt_node = root->bro_node = root->lch_node = NULL;
+	root = new RE_tree_node(Regex_start, NULL);
 }
 
+void RE_tree::input_regex()
+{
+	cout <<"请输入正则表达式："<<endl;
+	cin>> regex;
+}
+void RE_tree::generate_tree()
+{
+	string::iterator pos_current = regex.begin();
+	string::iterator pos_end = regex.end();
+	RE_tree_node * node = root;
+	while(true)
+	{
+
+	}
+}
+
+void RE_tree::push_element()
+{
+}
+void RE_tree::backtrack()
+{
+}
 int main()
-{}
+{
+	RE_tree re;
+	re.input_regex();
+	cout << re.regex << endl;
+}
--- RE_tree.h
+++ RE_tree.h
@@ -3,16 +3,20 @@
   文法：选择R|S；连接RS；闭包R*
 
 
-1    RegEx -> RegEx | A
-2          -> A
 
-3    A -> A B
-4      -> B
+
+     Regex' -> A' Regex
+1    Regex -> | A' Regex
+2           -> eof
+
+     A' -> B A
+3    A -> B A
+4      -> eof
 
 5    B -> C*
 6      -> C
 
-7    C -> ( RegEx )
+7    C -> ( RegEx' )
 8      -> e
 
   该正则表达式的字符包括a-z A-Z 0-9 和 _
@@ -26,39 +30,54 @@
 #include <iostream>
 #include <set>
 #include <vector>
+#include <string>
+#include <stack>
 using namespace std;
 
-//以下可以组织成一个类
+enum {
+	Regex_type = 0, A_type, B_type, C_type, Regex_start, A_start, e_type, sign_type
+};
 
 
-/*新的结构栈
-用于回溯*/
 
-struct RE_tree_node
+class RE_tree_node
 {
+public:
 	int type;
 	char value;
+	RE_tree_node *prt_node;
 	RE_tree_node *lch_node;
 	RE_tree_node *bro_node;
-	RE_tree_node *prt_node;
+	RE_tree_node(int t, RE_tree_node *p, char v = '\0', RE_tree_node *l = NULL, RE_tree_node *b = NULL);
 };
+
+struct stack_element
+{
+	RE_tree_node *node_inserted;
+	int type;
+};
+
 
 static void insert_production_one(int type, RE_tree_node *p, char num = '\0');
 static void insert_production_two(int type, RE_tree_node *p, char num = '\0');
 static void insert_production_three(int type, RE_tree_node *p, char num = '\0');
 static void insert_production_four(int type, RE_tree_node *p, char num = '\0');
-
+static void insert_production_five(int type, RE_tree_node *p, char num = '\0');
+static void insert_production_six(int type, RE_tree_node *p, char num = '\0');
 class RE_tree
 {
 public:
-	enum {
-		Regex_type = 0, A_type, B_type, C_type, e_type, sign_type
-	};
-	RE_tree();
-	set<char> terminator;
-	vector<char> regularExpression;
-	RE_tree_node *root;
 	typedef void (*insert_func)(int type, RE_tree_node *p, char num);
+	RE_tree();
+	void input_regex();
+	void push_element();
+	void backtrack();
+	void generate_tree();
+
+	stack<stack_element> record;
+	set<char> terminator;
+	string regex;
+	RE_tree_node *root;
 	vector<insert_func> insert;
 private:
 
